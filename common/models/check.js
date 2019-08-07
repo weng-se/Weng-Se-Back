118a91dfdbb0fb17cabf2ff40c6f57104d342e6f
younes belhit <younesbelhit@gmail.com>
2019-08-07 14:28:52 +0100

    "YO BE": update 8

--- common/models/check.js
+++ common/models/check.js
@@ -2,4 +2,34 @@
 
 module.exports = function(Check) {
 
+	Check.findAll = function(value, cb) {
+	    Check.find({ 
+	        where: { 
+	            status: {
+	            	neq: value
+	            }
+	        }
+	    }, function(err, response) {
+	        if (err) throw err;
+	        cb(null, response);
+	    });
+	};
+
+	Check.remoteMethod('findAll', {
+	    accepts: {
+	        arg: 'status',
+	        type: 'string',
+	        required: false
+	    },
+	    returns: {
+	        arg: 'response',
+	        type: 'array',
+	        root: true
+	    },
+	    http: {
+	        path: '/findAll',
+	        verb: 'get'
+	    }
+	});
+
 };
