a296e1df270c834d78d6f9bf8a7ecf2869450b6c
younes belhit <younesbelhit@gmail.com>
2019-09-03 16:12:19 +0100

    "YO BE" check

--- common/models/check.js
+++ common/models/check.js
@@ -32,4 +32,113 @@ module.exports = function(Check) {
 	    }
 	});
 
+
+
+	Check.getSumCheck = function (fromTime, toTime, cb) {
+
+	    // let filter = {
+	    //   where: {
+	    //     issuedDate: { gt: fromTime },
+	    //     issuedDate: { lt: toTime }
+	    //   }
+	    // };
+
+
+
+	    let filter = {
+		    where: {
+		       issuedDate: { between : [fromTime, toTime ]},
+		    }
+		};
+
+
+	    Check.find(filter, function(err, trafficResults) {
+
+	      if(err) return cb(err); // error out and return err msg to client
+
+	      let graphData = [];
+
+	      // loop on trafficResults to do Summation here
+	      // collect results inside graphData
+
+	      // return clean graphData to client
+	      for (var i = 0; i < trafficResults.length; i++) {
+			graphData.push(trafficResults[i].amount);
+		  }
+		  
+		  var count = 0;
+		  
+		  for(var i=0, n=graphData.length; i < n; i++) { 
+			count += graphData[i]; 
+		  }
+
+		  cb(null, count);
+
+	    });
+
+	  }
+
+	  Check.remoteMethod('getSumCheck', {
+	  	http: { 
+	  		path: '/getSumCheck', 
+	  		verb: 'get' 
+	  	},
+	    accepts: [
+	      { arg: 'fromTime', type: 'date' },
+	      { arg: 'toTime', type: 'date' }
+	    ],
+	    returns: { 
+	    	root: true,
+	    	type: 'array' 
+	    }
+	 
+	});
+
+
+
+
+
+	Check.getCountCheck = function (fromTime, toTime, cb) {
+
+		   let filter = {
+		     where: {
+		       issuedDate: { between : [fromTime, toTime ]},
+		       
+		     }
+		   };
+		
+   
+		   Check.find(filter, function(err, trafficResults) {
+   
+		     if(err) return cb(err); // error out and return err msg to client
+		
+			let count = trafficResults.length
+
+		     cb(null, count);
+   
+		   });
+
+   
+		 }
+   
+		 Check.remoteMethod('getCountCheck', {
+		   	accepts: [
+		     	{ arg: 'fromTime', type: 'date' },
+		     	{ arg: 'toTime', type: 'date' }
+		   	],
+		   	returns: { 
+		   		arg: 'count', 
+		   		type: 'number' 
+		   	},
+			http: {
+	        	path: '/getCountCheck',
+	        	verb: 'get'
+	    	}
+		
+	   });
+
+
+
+
+
 };
